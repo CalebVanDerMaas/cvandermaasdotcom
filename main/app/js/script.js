9961b624dcaa72a5b1835478106e5ef52e13dc3d
CrunchyLlama <calebvandermaasc@gmail.com>
2022-01-08 21:22:20 -0500

    added hamburger menu animation for mobile

--- main/app/js/script.js
+++ main/app/js/script.js
@@ -1,1 +1,13 @@
-console.log('Hello world!');
+const btnHamburger = document.querySelector('#btnHamburger');
+
+btnHamburger.addEventListener('click', function(){
+    console.log('open hamburger');
+
+    if(btnHamburger.classList.contains('header__open')){
+        btnHamburger.classList.remove('header__open');
+    }
+    else {
+      btnHamburger.classList.add('header__open');
+    }
+    
+});
